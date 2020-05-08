40577360b17145214416fe135539998b1687d421
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-05-08 12:13:24 +0200

    API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
-import x from "./test";
+// import string from "./modules/Search";
+//
+// import { add as a, multiply as m, id } from "./views/searchView";
+//
+// import * as searchView from "./views/searchView";
+//
+// console.log(`using imported function! ${a(id, 2)} and ${m(3, 5)}, ${string} `);
+//
+// console.log(
+//   `using imported function! ${searchView.add(id, 2)} and ${searchView.multiply(
+//     3,
+//     5
+//   )}, ${string} `
+// );
+import axios from "axios";
 
-console.log(`i importated ${x} called tet.js`);
+async function getResults(query) {
+  try {
+    const result = await axios(
+      `https://forkify-api.herokuapp.com/api/search?q=${query}`
+    );
+    const recipes = result.data.recipes;
+    console.log(recipes);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getResults("pizza");
