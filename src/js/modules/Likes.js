a8f9788355931fa9c6323e84ad6e4d091ef22ef5
Miguel Patrocinio <jose.m.patrocinio@hotmail.com>
2020-05-14 15:35:30 +0200

    building the likesModel

---
+++ src/js/modules/Likes.js
@@ -0,0 +1,27 @@
+export default class Likes {
+  constructor() {
+    this.likes = likes;
+  }
+
+  addLike(id, title, author, img) {
+    const like = {
+      id,
+      title,
+      author,
+      img
+    };
+    this.likes.push(likes);
+  }
+
+  deleteLike(id) {
+    const index = this.likes.findIndex(el => el.id === id);
+    this.likes.splice(index, 1);
+  }
+  isLiked(id) {
+    return this.likes.findIndex(el => el.id === id) !== -1;
+  }
+
+  getNumLikes() {
+    return this.likes.length;
+  }
+}
